1b6382414006133ac7a83bfdb8c894b4b8db3f18
Gideon Oba <deyonoba@gmail.com>
2024-01-30 22:05:48 +0000

    modified: 0-simple_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,18 @@
 #!/usr/bin/env ruby
-# Regular Expression matching School
+# Regular expression that matches School
 
-puts ARGV[0].scan(/School/).join
+def match_school(input)
+  regex = /School/
+  if match_data = input.match(regex)
+    puts match_data[0]
+  else
+    puts ''
+  end
+end
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <input>"
+  exit 1
+end
+
+match_school(ARGV[0])
