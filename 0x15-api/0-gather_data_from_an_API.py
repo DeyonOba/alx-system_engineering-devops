358cc3d80bbaa81336a416ae2d26d22815c6e99b
Gideon Oba <deyonoba@gmail.com>
2024-04-23 16:12:40 +0000

    refactor: 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,8 +17,9 @@ def get_todo_list_progress(employee_id):
     # store URL's
     web_url = "https://jsonplaceholder.typicode.com"
     user_var = "users"
-    user_data_url = "/".join([web_url, user_var, employee_id])
-    todo_list_url = "/".join([user_data_url, f"todos?userId={employee_id}"])
+    user_data_url = "/".join([web_url, user_var, str(employee_id)])
+    todo_list_url = "/".join(
+        [user_data_url, f"todos?userId={str(employee_id)}"])
 
     # Make user and to-do list requests
     user_request = requests.get(user_data_url)
